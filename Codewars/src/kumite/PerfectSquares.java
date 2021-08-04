b5f33630a8471212f980984abe0c83f5aa322309
VadimRudz <83311092+VadimRudz@users.noreply.github.com>
2021-08-04 14:14:46 +0200

    Add files via upload

---
+++ Codewars/src/kumite/PerfectSquares.java
@@ -0,0 +1,20 @@
+package kata;
+
+public class PerfectSquares {
+	
+	public static String findSquares(int n){
+			int big = n/2 + 1;
+			int small = n/2;
+			if(Math.pow(big,2)>2147483647){
+				long bigLong = (long) big;
+				long smallLong = (long) small;
+				return bigLong*bigLong + "-" + smallLong*smallLong;
+			}
+			return (int)Math.pow(big,2)+ "-" + (int)Math.pow(small,2);		
+	  }
+
+	public static void main(String[] args) {
+		System.out.println(findSquares(9));
+	}
+
+}
