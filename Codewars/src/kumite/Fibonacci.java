f7bc2a69b0a2a008c7eed89c366deba22be91ae4
VadimRudz <83311092+VadimRudz@users.noreply.github.com>
2021-08-22 18:33:16 +0300

    Update Fibonacci.java

--- Codewars/src/kumite/Fibonacci.java
+++ Codewars/src/kumite/Fibonacci.java
@@ -4,12 +4,28 @@ public class Fibonacci {
 
 	public static long fib (int n){
 		
-		return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+		if(n==1){	
+			return 1;
+		}
 		
-	}
-
+		else{
+			long AtStart = 0;
+			long AtEnd = 1;
+	
+			for(int i = 1; i < n; i++) {
+				long middleS = AtStart;
+				long middleE = AtEnd;
+				AtStart = middleE;
+				AtEnd = middleS + middleE;
+				System.out.println("Start - " + AtStart + "; End - " + AtEnd);
+					}
+			return AtEnd;
+		   }
+		}
+	
 	public static void main(String[] args) {
-
+		System.out.println(fib(5));
+		
 	}
 
 }
